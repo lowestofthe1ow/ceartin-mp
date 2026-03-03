0c0660d1305e562a42a201327766368402e17e9e
lowestofthe1ow <1owestoftheow@gmail.com>
2026-03-03 19:58:11 +0800

    Create Gemini prompt script

---
+++ src/scripts/query_gemini.py
@@ -0,0 +1,47 @@
+from typing import List
+
+from dotenv import dotenv_values
+from google import genai
+from pydantic import BaseModel
+
+from src.utils.generate_prompt import generate_prompt
+from src.utils.homographs import fill_template, homographs
+
+
+class Response(BaseModel):
+    """JSON schema for Gemini API's structured output"""
+
+    answers: List[str]
+
+
+# Set up Gemini API
+config = dotenv_values(".env")
+key = config["GEMINI_API_KEY"]
+client = genai.Client(api_key=key)
+
+# TODO: This is a sampple sentence
+SENTENCE = "Ang sikolohiya ay mahalaga sa edukasyon dahil ito'y nag-aaral ng mga proseso ng pag-aaral at pag-unlad ng isip at damdamin ng mga mag-aaral. Ito'y nagbibigay linaw sa mga guro kung paano turuan ang mga mag-aaral nang mas epektibo batay sa kanilang pangangailangan"
+
+# Extract homograph information given the sentence
+ambiguous_words, choices, output_template = homographs(SENTENCE)
+
+prompt = generate_prompt(
+    sentence=SENTENCE, pronunciations=choices, words=ambiguous_words
+)
+
+print("=" * 40)
+print("\nThe following prompt will be fed to Gemini:")
+print(prompt)
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview",
+    contents=prompt,
+    config={
+        "response_mime_type": "application/json",
+        "response_json_schema": Response.model_json_schema(),
+    },
+)
+
+output = Response.model_validate_json(response.text)
+
+print(fill_template(output_template, output.answers))
