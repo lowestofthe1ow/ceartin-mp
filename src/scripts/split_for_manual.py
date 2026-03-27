2c7b2790077e6faebb46cefcec0ffd15d550b15c
lowestofthe1ow <1owestoftheow@gmail.com>
2026-03-27 08:14:08 +0800

    Remove NewsPH-NLI from manual split

--- src/scripts/split_for_manual.py
+++ src/scripts/split_for_manual.py
@@ -9,7 +9,7 @@ tokenizer = AutoTokenizer.from_pretrained(MODEL_ID)
 
 dataset_files = [
     "data/tatoeba/phonetic_tatoeba_gemini_3.csv",
-    "data/newsph-nli/phonetic_newsph-nli_gemini_2.5_lite.csv",
+    #    "data/newsph-nli/phonetic_newsph-nli_gemini_2.5_lite.csv",
     "data/stress-minimal/stress-minimal_ambiguous_split.csv",
     "data/stress-minimal/stress-minimal_single_split.csv",
 ]
@@ -19,7 +19,7 @@ split_dataset = dataset_from_csv_list(dataset_files, tokenizer)
 test_split = split_dataset["test"]
 print(f"Original test set size: {len(test_split)} rows")
 
-sampled_test_dict = test_split.train_test_split(test_size=0.1695, seed=765)
+sampled_test_dict = test_split.train_test_split(test_size=0.2821, seed=765)
 sampled_test_10_percent = sampled_test_dict["test"]
 
 columns_to_keep = ["index", "sentence", "phoneme"]
@@ -27,5 +27,5 @@ sampled_test_10_percent = sampled_test_10_percent.select_columns(columns_to_keep
 
 print(f"Sampled size: {len(sampled_test_10_percent)} rows")
 
-output_path = "manual.csv"
+output_path = "data/manual_test.csv"
 sampled_test_10_percent.to_csv(output_path, index=False)
